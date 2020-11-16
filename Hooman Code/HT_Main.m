78a896435327f1cbf2538c0cddabbbe8f6d148b0
Hooman <hooman_t_p@bitbucket.org>
2020-11-16 16:42:38 -0500

    updated code to include V1 of drive control. Able to drive decently with drive errors turned on, but sometimes gets stuck on corners. Also commiting obstacle avoidance backup code

--- Hooman Code/HT_Main.m
+++ Hooman Code/HT_Main.m
@@ -23,7 +23,7 @@ global ir_circle
 
 %% User-editable variables and flags
 % Constants
-bot_center = [9.5,42];  % Robot starting location
+bot_center = [30,42];  % Robot starting location
 
 %%%%%% CODE ADDED BY HOOMAN %%%%%%%%%%%%
 % setting bot center to be a random location
@@ -54,12 +54,12 @@ bot_rot = 0;            % Robot starting rotation
 block_center = [25,41]; % Block starting location
 blocksize = 3;          % Block side length in inches
 num_segments = 10;      % Number of movement segments
-strength = [0, 0];	% How intense the random drive bias is, if enabled
+strength = [0.05, 1];	% How intense the random drive bias is, if enabled
 step_time = 0;          % Pause time between the algorithm executing commands
 
 % Control Flags and Setup
 randerror = 0;          % Use either a random error generator (1) or consistent error generation (0)
-randbias = 0;           % Use a randomized, normally distributed set of drive biases
+randbias = 1;           % Use a randomized, normally distributed set of drive biases
 sim = 1;                % Use the simulator (1) or connect to robot via blueteooth (0)
 plot_robot = 1;         % Plot the robot as it works its way through the maze
 plot_sense = 1;         % Plot sensor interactions with maze, if relevant
