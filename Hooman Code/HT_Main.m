30a8c1df06f67e9c7b326139eca4daa87ce7fdef
Hooman Tahmasebipour <hooman.tahmasebipour@mail.utoronto.ca>
2020-12-01 11:01:58 -0500

    fixed bugs with M3 trial1

--- Hooman Code/HT_Main.m
+++ Hooman Code/HT_Main.m
@@ -23,10 +23,10 @@ global ir_circle
 
 %% User-editable variables and flags
 % Constants
-bot_center = [42,6];  % Robot starting location
+bot_center = [66,42];  % Robot starting location
 
 bot_rot = 180;            % Robot starting rotation
-block_center = [14,34]; % Block starting location
+block_center = [18,30]; % Block starting location
 blocksize = 2;          % Block side length in inches
 num_segments = 10;      % Number of movement segments
 strength = [0, 0];	% How intense the random drive bias is, if enabled
